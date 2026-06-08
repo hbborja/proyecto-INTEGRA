2108ba8777a431f64339a73197ca1323f43d9ba0
0409LUIS <lgpozo1@espe.edu.ec>
2026-06-08 16:18:10 -0500

    servidor flask agregado usando pytho

---
+++ app.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def  loguin():
+    return render_template("index.html") # El nombre de tu HTML principal
+
+
+@app.route("/nomina")
+def pagina_nomina():
+    # Si tus otras páginas de nómina sí están en subcarpetas, aquí sí especificas la ruta:
+    return render_template("modules/nomina/index.html") # (O el nombre de tu archivo i
+
+@app.route("/contabilidad") # <-- La URL que se verá en el navegador
+def pagina_contabilidad():  # <-- ¡ESTE NOMBRE debe ser idéntico al del HTML!
+    
+    # Aquí pones la ruta real de tu HTML de contabilidad
+    return render_template("modules/contabilidad/index.html")
+
+@app.route("/mantenimiento") # <-- La URL que se verá en el navegador
+def pagina_mantenimiento():  # <-- ¡ESTE NOMBRE debe ser idéntico al del HTML!
+    
+    # Aquí pones la ruta real de tu HTML de mantenimiento
+    return render_template("modules/mantenimiento/index.html")
+
+@app.route("/biblioteca") # <-- La URL que se verá en el navegador
+def pagina_biblioteca():  # <-- ¡ESTE NOMBRE debe ser idéntico al del HTML!
+    
+    # Aquí pones la ruta real de tu HTML de biblioteca
+    return render_template("modules/biblioteca/index.html")
+
+# para nomina ul
+# 2. Opción: Motivos Ingreso/Egreso
+@app.route("/nomina/motivos")
+def pagina_motivos():  
+    return render_template("modules/nomina/motivos.html") 
+
+# 3. Opción: Gestión de Nómina
+@app.route("/nomina/gestion_nomina")
+def pagina_gestion_nomina():  
+    return render_template("modules/nomina/gestion_nomina.html") 
+
+# 4. Opción: Valores a Pagar
+@app.route("/nomina/reporte_valores")
+def pagina_reporte_valores():  
+    return render_template("modules/nomina/reporte_valores.html") 
+
+# 5. Opción: Reporte Cruzado
+@app.route("/nomina/reporte-cruzado")
+def pagina_reporte_cruzado():  
+    return render_template("modules/nomina/reporte_cruzado.html")
+
+
+# para contabilidad ul
+@app.route("/contabilidad/cuenta")
+def pagina_cuenta():
+    return render_template("modules/contabilidad/cuenta.html")
+
+@app.route("/contabilidad/comprobante")
+def pagina_comprobante():
+    return render_template("modules/contabilidad/comprobante.html")
+
+@app.route("/contabilidad/balance")
+def pagina_balance():
+    return render_template("modules/contabilidad/balance.html")
+
+@app.route("/contabilidad/estado_resultados")
+def pagina_resultados():
+    return render_template("modules/contabilidad/estado_resultados.html")
+
+# para mantenimiento ul
+@app.route("/mantenimiento/activos")
+def pagina_activos():
+    return render_template("modules/mantenimiento/activos.html")
+
+@app.route("/mantenimiento/reporte_cruzado")
+def pagina_reporte():
+    return render_template("modules/mantenimiento/reporte_cruzado.html")
+
+@app.route("/mantenimiento/reporte_valores")
+def pagina_valores():
+    return render_template("modules/mantenimiento/reporte_valores.html")
+
+@app.route("/mantenimiento/mantenimiento")
+def pagina_mantenimiento2():
+    return render_template("modules/mantenimiento/mantenimiento.html")
+
+#Biblioteca
+@app.route("/biblioteca/libros")
+def pagina_libros():
+    return render_template("modules/biblioteca/libros.html")
+
+@app.route("/biblioteca/asientos")
+def pagina_asientos():
+    return render_template("modules/biblioteca/asientos.html")
+
+@app.route("/biblioteca/prestamos")
+def pagina_prestamos():
+    return render_template("modules/biblioteca/prestamos.html")
+
+@app.route("/biblioteca/reportes")
+def pagina_reportes():
+    return render_template("modules/biblioteca/reportes.html")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
